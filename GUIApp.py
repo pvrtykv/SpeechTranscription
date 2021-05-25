7727f9c520b69000eca40dccf92ab306feedf120
pvrtykv <jotpe2307@gmail.com>
2021-05-25 20:57:54 +0200

    working on recording player feature

--- GUIApp.py
+++ GUIApp.py
@@ -1,6 +1,7 @@
 import binascii
 import hashlib
 import threading
+import time
 import tkinter as tk  # python 3
 import wave
 from tkinter import font as tkfont  # python 3
@@ -11,12 +12,9 @@ import re
 from tkinter import filedialog as fd
 from tkinter.scrolledtext import ScrolledText
 import tkinter.ttk as ttk
-
-
-
 import pyaudio
 import sounddevice as sd
-
+import pygame
 
 class RecordControl():
     def __init__(self):
@@ -198,12 +196,12 @@ class Register(tk.Frame):
 
         tk.Label(self, text="Please enter details below", bg="blue").pack()
         tk.Label(self, text="").pack()
-        username_lable = tk.Label(self, text="Username * ")
-        username_lable.pack()
+        username_label = tk.Label(self, text="Username * ")
+        username_label.pack()
         self.username_entry = tk.Entry(self, textvariable=self.username)
         self.username_entry.pack()
-        password_lable = tk.Label(self, text="Password * ")
-        password_lable.pack()
+        password_label = tk.Label(self, text="Password * ")
+        password_label.pack()
         self.password_entry = tk.Entry(self, textvariable=self.password, show='*')
         self.password_entry.pack()
         tk.Label(self, text="").pack()
@@ -242,6 +240,9 @@ class MainPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.is_paused = False
+        self.is_playing = False
+        self.is_started = False
         self.cwd = os.getcwd()
         self.filetypes = [('text files', '.txt')]
         label = tk.Label(self, text="this is main page", font=controller.title_font)
@@ -252,7 +253,7 @@ class MainPage(tk.Frame):
 
         button_recorder = tk.Button(self, text="Record", command=self.record)
         button_recorder.pack()
-        button_player = tk.Button(self, text="Play")
+        button_player = tk.Button(self, text="Play", command=self.play)
         button_player.pack()
         button_reader = tk.Button(self, text="Open text file", command=self.open_file)
         button_reader.pack()
@@ -274,6 +275,58 @@ class MainPage(tk.Frame):
         tk.Button(record_screen, text="STOP",
                   command=lambda: self.delete_screen(record_screen, record_control, thread)).pack()
 
+    def play(self):
+        audio = fd.askopenfilename(title="Open wav file", initialdir=self.cwd + '/media')
+        if audio:
+            play_screen = tk.Toplevel(self)
+            play_screen.title("Play")
+            play_screen.geometry("200x200")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(audio)
+
+
+            controls_frame = tk.Frame(play_screen)
+            controls_frame.grid(pady=20)
+
+            play_btn_img = tk.PhotoImage(file="images/play.png")
+            pause_btn_img = tk.PhotoImage(file="images/pause.png")
+            stop_btn_img = tk.PhotoImage(file="images/stop.png")
+
+            play_btn = tk.Button(controls_frame, image=play_btn_img, command=self.play_audio)
+            play_btn.image = play_btn_img
+            pause_btn = tk.Button(controls_frame, image=pause_btn_img, command=self.pause)
+            pause_btn.image = pause_btn_img
+            stop_btn = tk.Button(controls_frame, image=stop_btn_img, command=self.stop)
+            stop_btn.image = stop_btn_img
+
+            play_btn.grid(row=0, column=0, padx=10)
+            pause_btn.grid(row=0, column=1, padx=10)
+            stop_btn.grid(row=0, column=2, padx=10)
+
+    def play_audio(self):
+        if self.is_playing is False:
+            if self.is_paused:
+                pygame.mixer.music.unpause()
+                self.is_paused = False
+                self.is_playing = True
+            else:
+                pygame.mixer.music.play()
+                time.sleep(2)
+                self.is_playing = True
+
+    def stop(self):
+        if self.is_playing:
+            pygame.mixer.music.stop()
+            self.is_started = False
+
+
+    def pause(self):
+        if self.is_playing:
+            pygame.mixer.music.pause()
+            time.sleep(2)
+            self.is_paused = True
+
     def delete_screen(self, screen, record_control, thread):
         record_control.finished = True
         thread.join()
@@ -283,7 +336,7 @@ class MainPage(tk.Frame):
         screen.destroy()
 
     def open_file(self):
-        file = fd.askopenfilename(title="Open text file", initialdir='/', filetypes=self.filetypes)
+        file = fd.askopenfilename(title="Open text file", initialdir=self.cwd, filetypes=self.filetypes)
         if file:
             file_screen = tk.Toplevel(self)
             text = ScrolledText(file_screen, height=30, width=30)
@@ -295,49 +348,46 @@ class MainPage(tk.Frame):
 
     def transcribe(self):
         file = fd.askopenfilename(title="Open wav file", initialdir=self.cwd+'/media')
+        if file:
+            transcribe_screen = tk.Toplevel(self)
+            text = ScrolledText(transcribe_screen, height=30, width=30)
+            text.pack()
 
-        transcribe_screen = tk.Toplevel(self)
-        text = ScrolledText(transcribe_screen, height=30, width=30)
-        text.pack()
+            file_list = open("julius/test.dbl", 'w')
+            file_list.write(file)
+            file_list.close()
 
-        file_list = open("julius/test.dbl", 'w')
-        file_list.write(file)
-        file_list.close()
+            if not os.path.exists('julius_output'):
+                os.mkdir('julius_output')
 
-        if not os.path.exists('julius_output'):
-            os.mkdir('julius_output')
+            output = utils.increment_filename("julius_output/output.txt")
 
-        output = utils.increment_filename("julius_output/output.txt")
+            subprocess.run(["julius-dnn", "-C", "julius.jconf", "-dnnconf", "dnn.jconf", ">", "../" + output],
+                           shell=True, cwd="julius",
+                           check=True)
 
-        subprocess.run(["julius-dnn", "-C", "julius.jconf", "-dnnconf", "dnn.jconf", ">", "../" + output],
-                       shell=True, cwd="julius",
-                       check=True)
+            r = re.compile(r"sentence1: <s> (.+?) </s>")
 
-        r = re.compile(r"sentence1: <s> (.+?) </s>")
+            f1 = open(output, 'r')
+            lines = f1.readlines()
+            f1.close()
 
-        f1 = open(output, 'r')
-        lines = f1.readlines()
-        f1.close()
+            if not os.path.exists('transcriptions'):
+                os.mkdir('transcriptions')
 
-        if not os.path.exists('transcriptions'):
-            os.mkdir('transcriptions')
+            transcription = utils.increment_filename("transcriptions/transcription.txt")
+            f2 = open(transcription, 'a')
 
-        transcription = utils.increment_filename("transcriptions/transcription.txt")
-        f2 = open(transcription, 'a')
+            for line in lines:
+                c = r.match(line)
+                if c is not None:
+                    f2.write("\n")
+                    f2.write(c.group(1))
 
-        for line in lines:
-            c = r.match(line)
-            if c is not None:
-                f2.write("\n")
-                f2.write(c.group(1))
-
-        f2.close()
-        with open(transcription, 'r') as f:
-            text.insert(tk.END, f.read())
-        text.pack()
-
-
-
+            f2.close()
+            with open(transcription, 'r') as f:
+                text.insert(tk.END, f.read())
+            text.pack()
 
 
 if __name__ == "__main__":
